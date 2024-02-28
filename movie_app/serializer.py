8ed334799852fcbf52ab59961d4af36491a6d188
Aidarkg <adijamgyrchiev@gmail.com>
2024-02-28 16:45:04 +0600

    hw2

--- movie_app/serializer.py
+++ movie_app/serializer.py
@@ -2,11 +2,19 @@ from rest_framework import serializers
 from movie_app import models
 
 class DirectorSerializer(serializers.ModelSerializer):
+    movies = serializers.SerializerMethodField()
     movies_count = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Director
         fields = '__all__'
+
+    def get_movies(self, director):
+        movies = director.movie.all()
+        if movies:
+            return ', '.join([movie.title for movie in movies])
+        else:
+            return "No movies"
 
     def get_movies_count(self, director):
         return director.movie.count()
@@ -19,17 +27,25 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    directors = DirectorSerializer(many=True, read_only=True)
-    reviews = ReviewSerializer(many=True, read_only=True)
+    directors = serializers.SerializerMethodField()
     rating = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Movie
         fields = '__all__'
 
+
+    def get_directors(self, movie):
+        directors = movie.directors.all()
+        if directors.exists():
+            return ', '.join([director.name for director in directors])
+        else:
+            return 'No directors'
+        
+
     def get_rating(self, movie):
-        total_stars = sum(review.stars for review in movie.reviews.all())
-        num_reviews = movie.reviews.count()
+        total_stars = sum(review.stars for review in movie.review.all())
+        num_reviews = movie.review.count()
         if num_reviews > 0:
             return total_stars / num_reviews
         else:
